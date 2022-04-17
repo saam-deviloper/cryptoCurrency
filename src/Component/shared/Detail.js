36252e75a0346487e5262b7e882360118d280676
saam <sami258000@gmail.com>
2022-04-18 00:30:27 +0430

    base

--- src/Component/shared/Detail.js
+++ src/Component/shared/Detail.js
@@ -13,9 +13,9 @@ export default function Detail() {
   return (
     <Container>
       <div className="mt-5"><Link to={'../'} className='text-decoration-none d-inline ' >Home</Link><img src="https://img.icons8.com/small/16/000000/more-than.png" alt="#"/><p className="text-muted d-inline">{` ${name}`}</p></div>
-      <div className="row justify-content-center align-items-center mt-5 mb-2" >
-      <img src={image} height='70px' className='col-xs-6 col-sm-12 col-md-1 col-lg-1'
-      /> <h1 className="display-5  col-xs-6 col-sm-12 col-md-1 col-lg-1">{name}</h1>
+      <div className="d-flex  align-items-center mt-5 mb-2" >
+      <img src={image} height='70px' className=''
+       alt='logo'/> <h1 className="display-5  ">{name}</h1>
       </div>
       <h3 className="bold" ><strong>{current_price}$</strong></h3>
       <ProgressBar animated min={+low_24h} variant='warning' max={+high_24h} now={50} className='w-50 ' />
